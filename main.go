a7d56bff4d6647cd2c3bdc6b439e6d7386175abc
Timothy Jones <timothy.jones@rmit.edu.au>
2013-06-18 18:24:19 +1000

    Added comments to describe the R code

--- main.go
+++ main.go
@@ -37,19 +37,25 @@ func main() {
     fmt.Print(path.Base(systems[i]), " <- read.csv(sep=\" \",header=FALSE,file=\"",systems[i],"\")")
     fmt.Println()
   }
+  // Vectors to hold the data frame we're about to build
   fmt.Println("names <- c()")
   fmt.Println("AsigBetter <- c()")
   fmt.Println("BsigBetter <- c()")
   for i := 0; i < numSystems; i++ {
     for j := i+1 ; j < numSystems; j++ {
+      // First column is the names of the systems under test
       fmt.Print("names <- append(names,paste(\"",path.Base(systems[i]),"\",\"", path.Base(systems[j]),"\",sep=\" & \"))")
       fmt.Println()
+      // Next column is "yes" if system A is significantly better than B, "no" otherwise
       fmt.Print("AsigBetter <- append(AsigBetter,if(t.test(",path.Base(systems[i]),"$V2,",path.Base(systems[j]),"$V2,paired=TRUE)$p.value < 0.05) { if(mean(",path.Base(systems[i]),"$V2) > mean(",path.Base(systems[j]),"$V2)) { \"yes\"  } else {\"no\"} } else { \"no\" })")
       fmt.Println()
+      // Next column is "yes" if system B is significantly better than A, "no" otherwise
       fmt.Print("BsigBetter <- append(BsigBetter,if(t.test(",path.Base(systems[i]),"$V2,",path.Base(systems[j]),"$V2,paired=TRUE)$p.value < 0.05) { if(mean(",path.Base(systems[i]),"$V2) < mean(",path.Base(systems[j]),"$V2)) { \"yes\"  } else {\"no\"} } else { \"no\" })")
       fmt.Println()
     }
   }
+  // Create data frame we've just built
   fmt.Println("table <- data.frame(names,AsigBetter,BsigBetter)")
+  // For now, just print the table
   fmt.Println("table");
 }
