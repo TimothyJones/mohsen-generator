bf72a0125df0322b91169998e5846d512fdfab19
Timothy Jones <timothy.jones@rmit.edu.au>
2013-07-13 16:20:28 +1000

    Removed final value from input (it's a summary), and added path for ommitting zeroes from both engines from the statistics tests

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 )
 
 func main() {
+  noZeros := 0
   if len(os.Args) != 2 {
     fmt.Println(os.Args[0], ": generates an R script for producing a file in Mohsen's format")
     fmt.Println("Usage:")
@@ -36,7 +37,10 @@ func main() {
   for i := 0; i < numSystems; i++ {
     fmt.Print(path.Base(systems[i]), " <- read.csv(sep=\" \",header=FALSE,file=\"",systems[i],"\")")
     fmt.Println()
+    fmt.Print(path.Base(systems[i]), " <- ", path.Base(systems[i]),"$V1[1:50]")
+    fmt.Println()
   }
+  //fmt.Println("exactRankTests")
   // Vectors to hold the data frame we're about to build
   fmt.Println("names <- c()")
   fmt.Println("AsigBetter <- c()")
@@ -47,10 +51,21 @@ func main() {
       fmt.Print("names <- append(names,paste(\"",path.Base(systems[i]),"\",\"", path.Base(systems[j]),"\",sep=\" & \"))")
       fmt.Println()
       // Next column is "yes" if system A is significantly better than B, "no" otherwise
-      fmt.Print("AsigBetter <- append(AsigBetter,if(t.test(",path.Base(systems[i]),"$V1,",path.Base(systems[j]),"$V1,paired=TRUE)$p.value < 0.05) { if(mean(",path.Base(systems[i]),"$V1) > mean(",path.Base(systems[j]),"$V1)) { \"yes\"  } else {\"no\"} } else { \"no\" })")
+      // Let's try for no zeroes
+      fmt.Print("frame <- data.frame(",path.Base(systems[i]),",",path.Base(systems[j]),")")
+      fmt.Println()
+      if noZeros == 1 {
+        fmt.Print("frame <- subset(frame, ",path.Base(systems[i])," != 0 & ",path.Base(systems[j])," != 0)")
+        fmt.Println()
+      }
+      fmt.Print("AsigBetter <- append(AsigBetter,if(t.test(frame$",path.Base(systems[i]),",frame$",path.Base(systems[j]),",paired=TRUE)$p.value < 0.05) { if(mean(frame$",path.Base(systems[i]),") > mean(frame$",path.Base(systems[j]),")) { \"yes\"  } else {\"no\"} } else { \"no\" })")
+//      fmt.Print("AsigBetter <- append(AsigBetter,if(perm.test(",path.Base(systems[i]),",",path.Base(systems[j]),",paired=TRUE)$p < 0.05) { if(mean(",path.Base(systems[i]),") > mean(",path.Base(systems[j]),")) { \"yes\"  } else {\"no\"} } else { \"no\" })")
+
+
       fmt.Println()
       // Next column is "yes" if system B is significantly better than A, "no" otherwise
-      fmt.Print("BsigBetter <- append(BsigBetter,if(t.test(",path.Base(systems[i]),"$V1,",path.Base(systems[j]),"$V1,paired=TRUE)$p.value < 0.05) { if(mean(",path.Base(systems[i]),"$V1) < mean(",path.Base(systems[j]),"$V1)) { \"yes\"  } else {\"no\"} } else { \"no\" })")
+     fmt.Print("BsigBetter <- append(BsigBetter,if(t.test(frame$",path.Base(systems[i]),",frame$",path.Base(systems[j]),",paired=TRUE)$p.value < 0.05) { if(mean(frame$",path.Base(systems[i]),") < mean(frame$",path.Base(systems[j]),")) { \"yes\"  } else {\"no\"} } else { \"no\" })")
+ //     fmt.Print("BsigBetter <- append(BsigBetter,if(perm.test(",path.Base(systems[i]),",",path.Base(systems[j]),",paired=TRUE)$p < 0.05) { if(mean(",path.Base(systems[i]),") < mean(",path.Base(systems[j]),")) { \"yes\"  } else {\"no\"} } else { \"no\" })")
       fmt.Println()
     }
   }
